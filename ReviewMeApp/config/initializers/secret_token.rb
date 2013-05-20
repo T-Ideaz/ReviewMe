970eb4a06a928b08cc90f129295edfcbd27b7259
Nii Tetteh <niiashikwei@hotmail.com>
2013-05-20 00:01:55 -0400

    Nii : first commit, generated app using rails 3 and ruby 1.9.2

---
+++ ReviewMeApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReviewMeApp::Application.config.secret_token = '4f9db3152b0cb12f4df27ec62341ff264f7ba35c56f0f8b5216b33ed73f0daaf52d30b3d6e675866cbe25792136634d8b48b27745f13313ec8a1e9b71c05ab31'
